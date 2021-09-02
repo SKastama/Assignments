938dd5dfe8ebd57be71fbda856ca758068ec9f70
Sarah Kastama <sarahkastama@gmail.com>
2021-09-02 12:49:42 -0700

    commit

---
+++ big_inversion/src/components/PersonCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{props.title}</h1>
+            <p>Age: {props.age}</p>
+            <p>Hair-Color: {props.color}</p>
+        </div>
+    );
+}
+export default PersonCard;

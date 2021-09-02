d1f91f933c4e155b03fa019692f380d4513f05b6
Sarah Kastama <sarahkastama@gmail.com>
2021-09-02 15:04:33 -0700

    commit

---
+++ hook_form/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import UserInfo from "./components/UserInfo"
+
+function App() {
+  return (
+    <div className="App">
+      <UserInfo/>
+    </div>
+  );
+}
+
+export default App;

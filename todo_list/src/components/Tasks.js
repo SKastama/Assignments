32526b1d38751d39e07502e3d4045f0593b3f301
Sarah Kastama <sarahkastama@gmail.com>
2021-09-06 18:02:01 -0700

    commit

--- todo_list/src/components/Tasks.js
+++ todo_list/src/components/Tasks.js
@@ -2,37 +2,68 @@ import React, { useState } from 'react';
 
 const Tasks = (props) => {
     const [newTask, setNewTask] = useState("");
-    const [task, setTasks] = useState([]);
-    const [newTaskError, setNewTaskError] = useState("");
+    const [tasks, setTasks] = useState([]);
+    
 
     const createTask = (e) => {
         e.preventDefault();
-        const addNewTask = {newTask}
+        if (newTask.length < 1) {
+            return;
+        }
+        const taskItem = {
+            text: newTask,
+            complete: false
+        };
+        setTasks([...tasks, taskItem]);
         setNewTask("");
     }
+    const taskComplete = (idx) => {
+        const updatedTasks = tasks.map((task, i) =>{
+            if (idx === i) {
+                task.complete = !task.complete;
+                // const updatedTask = {...task, complete: !task.complete};
+                // return updatedTask;
+            }
+            return task;
+        })
+        setTasks(updatedTasks);
+    }
 
-    const valTask = (e) => {
-        setNewTask(e.target.value);
-        setTasks([...Tasks, newTask]);
-        if (e.target.value.length < 1) {
-            setNewTaskError("Task is required");
-        } else {
-            setNewTaskError("");
-        }
+    const deleteTask = (delIdx) => {
+        const filteredTasks = tasks.filter((task, i) => {
+            return i !== delIdx;
+        });
+        setTasks(filteredTasks);
     }
 
     return (
-        <form onSubmit={ (e) => e.preventDefault() }>
-            <div>
-                <input type="text" onChange={ valTask }  value={newTask}/>
-                {
-                    newTaskError ?
-                    <p style={{color:"red"}}>{ newTaskError }</p> :
-                    ""
-                }
-            </div>
-            <input type="submit" onClick={createTask} value="Add"/>
-        </form>
+        <div>
+            <form onSubmit={ (e) => createTask(e) }>
+                <div>
+                    <input type="text" value={newTask} onChange={ (e) => {
+                        setNewTask(e.target.value);
+                    }}/>
+                </div>
+                <input type="submit" onClick={createTask} value="Add"/>
+            </form>
+            {tasks.map((task, i) => {
+                return (
+                    <div key={i}>
+                        {
+                            task.complete ?
+                            <span style={{ textDecoration: "line-through" }}>{task.text} </span> :
+                            <span>{task.text} </span>
+                        }
+                        <input type="checkbox" checked={task.complete} onChange={(e) => {
+                            taskComplete(i);
+                        }}/>
+                        <button onClick={(e) => {
+                            deleteTask(i);
+                        }}>Delete</button>
+                    </div>
+                );
+            })}
+        </div>
     );
 }
 

d1b804f3cf74e177094e1686c9aaa2c0c75ee0e6
Sarah Kastama <sarahkastama@gmail.com>
2021-09-07 16:50:14 -0700

    commit

--- axios_pokemon_api/src/App.js
+++ axios_pokemon_api/src/App.js
@@ -4,17 +4,20 @@ import './App.css';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
-
-  const handlePokemon = (e) => {
-    axios.get("https://pokeapi.co/api/v2/pokemon")
+  const handlePokemon = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151")
       // .then(response => {response.json(e)})
-      .then(response => {setPokemon(response.results)})
+      .then(response => {setPokemon(response.data.results)})
   }
   return (
     <div className="App">
       <button onClick={handlePokemon}>Fetch</button>
-      {pokemon.length > 0 && pokemon.map((pokemon, index)=>{
-        return (<div key={index}>{pokemon.name}</div>)
+      {pokemon.map((pokemon) => {
+        return (
+          <div>
+            <span>{pokemon.name}</span>
+          </div>
+        )
       })}
     </div>
   );
